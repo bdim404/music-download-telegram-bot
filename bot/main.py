6b806f876684d78c80f3b79523109eed2cbc5dfc
bdim404 <i@bdim.moe>
2025-12-23 15:04:58 +0800

    Fix logging format by removing the logger name from the output

--- bot/main.py
+++ bot/main.py
@@ -23,9 +23,10 @@ from .handlers.error import error_handler
 
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    format='%(asctime)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
+logging.getLogger('httpx').setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 
